3783943b259e9403ebfdc38dfd975316e27f4965
Katrina Brinson <kbrinso@gmail.com>
2018-01-06 18:21:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === null){
+  if (cardNumber === NaN){
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
     const cartTotal = total();
